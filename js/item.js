d7372df62d8c5b146b39940f17effadd0e53d158
paein1029 <notouch18@naver.com>
2019-09-26 16:08:55 +0900

    item 추가

---
+++ js/item.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+
+	$('.itemlist').bxSlider({
+		minSlides:1,
+		maxSlides:7,
+		slideWidth:200,
+		slideMargin:80,
+		pager:false,
+			onSliderLoad:function(){
+			
+			$('.dragList').css("opacity", "1");
+
+		}
+
+	});
+   var droptext;
+  $( ".drag" ).draggable({
+  	 drag: function( event, ui ) {
+        droptext = $(this).text();
+         console.log("drag :"+$(this).text());
+      },
+     start: function( event, ui ) {
+     	$('.bx-viewport').css("overflow","visible");
+     	$(this).css("z-index","9999");
+         },
+     stop: function( event, ui ) {
+     	 var el = $(this);
+          // console.log(el);
+          el.css({
+            top: 0,left: 0
+          });
+     }
+  });
+
+
+   $( ".droppable" ).droppable({
+	   	over:function(event,ui){
+		    console.log("over");
+		    $(this).addClass("hovered");
+		},
+		out:function(event,ui){
+	        $(this).removeClass("hovered"); 
+	    },
+
+	    drop: function( event, ui ) {
+	    	$(this).removeClass("hovered"); 
+	    	console.log("drop : "+droptext);
+	    	$(this).find('span').text(droptext);
+	    }
+
+   	});
+});
